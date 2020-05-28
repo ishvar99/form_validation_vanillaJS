6f8c76ecb53667b76fab66c2b88386a0f7963c35
ishvar99 <ishanvarshney99@gmail.com>
2020-05-28 20:19:21 +0530

    added more checks

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ const email=document.getElementById('email');
 const password=document.getElementById('password');
 const password2=document.getElementById('password2');
 
-function checkValidEmail(email){
+function checkValidEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(String(input.value).toLowerCase())){
+        showSuccess(input);
+    }
+    else{
+        showError(input,'Email is not valid!');
+    }
 }
 
 function getFieldName(input){
-    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+    if(input.id=='password2'){
+        return '';
+    }
+    return `${input.id.charAt(0).toUpperCase()+input.id.slice(1)} is required`;
 }
 
 function showError(input,message){
@@ -24,15 +32,32 @@ function showSuccess(input){
     formControl.className='form-control success'
 }
 
-function checkList(inputArr){
+function checkRequired(inputArr){
     inputArr.forEach((input)=>{
         if(input.value.trim()===''){
-            showError(input,`${getFieldName(input)} is required`)
+            showError(input,`${getFieldName(input)}`)
         }
     })
 }
-
+function checkLength(input,min,max){
+    if(input.value.length<min)
+    {
+        showError(input,`${getFieldName(input)} must be at least ${min}`);
+    }
+    else if(input.value.length>max){
+        showError(input,`${getFieldName(input)} must be less than ${max}`)
+    }
+}
+function comparePasswords(password,password2){
+    if(password!==password2){
+        showError(password2,"passwords don/'t match")
+    }
+}
 form.addEventListener('submit',(e)=>{
    e.preventDefault();
-   checkList([username,email,password,password2]);
+   checkLength(username,3,15);
+   checkValidEmail(email);
+   checkLength(password,6,25);
+   comparePasswords(password,password2);
+   checkRequired([username,email,password,password2]);
 })
